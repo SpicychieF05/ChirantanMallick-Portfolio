dc5c9144f41a1daea5ea36140381ee819faa6a7b
spicychief05 <mallickchirantan@gmail.com>
2025-08-09 00:18:39 +0530

    Update resume download link in Contact section

--- client/src/components/sections/contact.tsx
+++ client/src/components/sections/contact.tsx
@@ -249,7 +249,7 @@ export default function Contact() {
             {/* Resume Download */}
             <div className="text-center">
               <a
-                href="https://drive.google.com/uc?export=download&id=1QqQ_Wkx8LxN6IeuDaYHx2s3q2zzQHfRs"
+                href="https://drive.google.com/file/d/1lPIXPhbDmuDqU1IGBc5DOoPKtbKeT6nG/view?usp=sharing"
                 className="gradient-border group inline-block"
                 download="Chirantan_Mallick_Resume.pdf"
               >
